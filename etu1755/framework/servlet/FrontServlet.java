722320e4963aa3a4dc0212435e9c8b278115a57e
notiaa <120126861+notiaa@users.noreply.github.com>
2023-03-27 08:25:02 +0300

    sprint2

--- etu1755/framework/servlet/FrontServlet.java
+++ etu1755/framework/servlet/FrontServlet.java
@@ -2,14 +2,18 @@ package etu1755.framework.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
-
+import etu1755.framework.Mapping;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class FrontServlet extends HttpServlet{
+
+    HashMap<String,Mapping> mappingurl;
+
 
 protected void processRequest(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
     response.setContentType("essaie/html;charset=UTF-8");
